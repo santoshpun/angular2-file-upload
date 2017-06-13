a527a2b1730a877a8332d04f0a475626faf64a54
santosh <santosh8pun@gmail.com>
2017-06-13 20:09:00 +0545

    updated

--- README.md
+++ README.md
@@ -5,12 +5,17 @@
     1. Latest version of Node to be installed.
 	
 ## Steps to Run
-	1. Run server
-		cd server				change directory to server
-		npm install          	install all the npm Dependencies
-		node app.js        	 	run node server
-	2. Run client
-		cd client			 	change directory to client
-		npm install          	install all the npm Dependencies
-		npm start        	 	run client
+	######1. Run server
+		cd server			
+		- change directory to server
+		npm install         
+		- install all the npm Dependencies
+		node app.js        	run node server
+	######2. Run client
+		cd client			
+		- change directory to client
+		npm install         
+		- install all the npm Dependencies
+		npm start        	
+		- run client
     
--- server/app.js
+++ server/app.js
@@ -17,25 +17,25 @@ var storage = multer.diskStorage({
 	},
 	filename: function (req, file, callback) {
 		var datetimestamp = Date.now();
-        callback(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1]);
+		callback(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1]);
 	}
 });
 
 var upload = multer({
-		 storage: storage,
-		 //check extension of uploaded files
-		 fileFilter: function (req, file, cb) {
-		 	//only image format file are supported
-		 	var filetypes = /jpeg|jpg/;
-		    var mimetype = filetypes.test(file.mimetype);
-		    var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+	storage: storage,
+	//check extension of uploaded files
+	fileFilter: function (req, file, cb) {
+		//only image format file are supported
+		var filetypes = /jpeg|jpg/;
+		var mimetype = filetypes.test(file.mimetype);
+		var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
 
-		    if (mimetype && extname) {
-		      return cb(null, true);
-		    }
-		    cb("Error: File upload only supports the following filetypes - " + filetypes);
-		 }
-		}).single('file');
+		if (mimetype && extname) {
+			return cb(null, true);
+		}
+		cb("Error: File upload only supports the following filetypes - " + filetypes);
+	}
+}).single('file');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -54,39 +54,25 @@ app.post('/api/photo', function (req, res) {
 	//console.log(req.file.ext);
 
 	upload(req, res, function (err) {
-        if (err) {
-            res.json({error_code: 1, err_desc: err, message: 'Error while processing your request'});
-            return;
-        }
+		if (err) {
+			res.json({ error_code: 1, err_desc: err, message: 'Error while processing your request' });
+			return;
+		}
 
-        console.log(req.file);
+		console.log(req.file);
 
-        fs.stat('./public/uploads/'+req.file.filename, function (err, stats) {
-		   if (err) {
-		       return console.error(err);
-		   }
+		fs.stat('./public/uploads/' + req.file.filename, function (err, stats) {
+			if (err) {
+				return console.error(err);
+			}
 
-		   var dimensions = sizeOf('./public/uploads/'+req.file.filename);
-		   console.log(dimensions.width, dimensions.height);
+			var dimensions = sizeOf('./public/uploads/' + req.file.filename);
+			console.log(dimensions.width, dimensions.height);
 
-		   console.log('Image uploaded successfully.');
-
-        		res.json({error_code: 0, err_desc: null, filename: req.file.filename});
-
-		   //check if image dimension match
-		   // if(dimensions.width == 2880 && dimensions.height == 1800){
-		   // 		console.log('Image uploaded successfully.');
-
-     //    		res.json({error_code: 0, err_desc: null, filename: req.file.filename});
-		   // }else{
-		   // 		 fs.unlink('./public/uploads/'+req.file.filename,function(err){
-				 //        if(err) return console.log(err);
-				 //        console.log('file deleted successfully');
-				 //   });  
-		   // 		 res.json({error_code: 1, err_desc: "Image dimension must be 2880x1800", message: "Image dimension must be 2880x1800"});
-		   // }
+			console.log('Image uploaded successfully.');
+			res.json({ error_code: 0, err_desc: null, filename: req.file.filename });
 		});
-    });
+	});
 });
 
 
